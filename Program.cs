45e24221aa1a37338eade7007068433ce0654e62
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-14 19:40:39 -0300

    Exercicio Cebolinha

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio_Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+           Console.Beep(); 
+            
+            String Cebolinha;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---------------Fale Igual ao Cebolinha da Turma da Monica---------------");
+            Console.ResetColor();
+            Console.Write("Escreva algo: ");
+            Cebolinha = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{Cebolinha.Replace("r","l").Replace("R","L")}");
+            Console.ResetColor();
+           
+        
+        }
+    }
+}
